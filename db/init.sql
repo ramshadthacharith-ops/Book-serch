df0858365a0909762cedff30c22f3b3cc6b9eec8
Ramshad Thacharoth <ramshadthacharith@gmail.com>
2026-06-29 09:59:14 +0000

    Add new book data

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS books (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL
+    title VARCHAR(255),
+    author VARCHAR(255)
 );
 
 INSERT INTO books (title, author) VALUES
-('Linux Basics', 'John'),
-('Python Guide', 'Guido'),
-('DevOps Handbook', 'Kim');
+('The Alchemist', 'Paulo Coelho'),
+('1984', 'George Orwell'),
+('Clean Code', 'Robert Martin'),
+('Your New Book', 'New Author');
